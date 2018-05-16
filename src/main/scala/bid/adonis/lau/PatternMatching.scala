4d92031d3e4b314fda61151ed2128e1163a6f026
Adonis <adonis.lau.work@gmail.com>
2018-05-16 18:46:41 +0800

    pattern matching

---
+++ src/main/scala/bid/adonis/lau/PatternMatching.scala
@@ -0,0 +1,42 @@
+package bid.adonis.lau
+
+/**
+  * 模式匹配
+  */
+object Matching {
+
+  def matchFun(x: Any): Any = x match {
+    case 1 => "one"
+    case "two" => 2
+    case y: Int => "scala.Int"
+    case _ => "many"
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(matchFun("two"))
+    println(matchFun("test"))
+    println(matchFun(1))
+    println(matchFun(6))
+  }
+}
+
+object CaseClasses {
+
+  //样例类
+  case class Person(name: String, age: Int)
+
+  def main(args: Array[String]) {
+    val alice = Person("Alice", 25)
+    val bob = Person("Bob", 32)
+    val charlie = Person("Charlie", 32)
+
+    for (person <- List(alice, bob, charlie)) {
+      person match {
+        case `alice` => println("Hi Alice!")
+        case Person("Bob", 32) => println("Hi Bob!")
+        case Person(name, age) => println("Age: " + age + " year, name: " + name + "?")
+      }
+    }
+  }
+
+}
