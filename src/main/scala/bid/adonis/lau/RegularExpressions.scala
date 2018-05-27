50cbe0ff0260f8be38da576d2a86296226d1895e
adonis <adonis.lau.dev@gmail.com>
2018-05-27 23:46:22 +0800

    正则表达式

---
+++ src/main/scala/bid/adonis/lau/RegularExpressions.scala
@@ -0,0 +1,24 @@
+package bid.adonis.lau
+
+import scala.util.matching.Regex
+
+object RegularExpressions {
+
+  def main(args: Array[String]): Unit = {
+    val pattern1 = "Scala".r
+    val str1 = "Scala is Scalable and cool"
+    println(pattern1 findFirstIn str1)
+
+    val str2 = "Scala is scalable and cool"
+    val pattern2 = new Regex("(S|s)cala") // 首字母可以是大写 S 或小写 s
+    println((pattern2 findAllIn str2).mkString(","))
+
+    val pattern3 = "(S|s)cala".r
+    val str3 = "Scala is scalable and cool"
+    println(pattern3 replaceFirstIn(str3, "Java"))
+
+    val pattern4 = new Regex("abl[ae]\\d+")
+    val str4 = "ablaw is able1 and cool"
+    println((pattern4 findAllIn str4).mkString(","))
+  }
+}
