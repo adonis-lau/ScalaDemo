a4150064bb309ed2f38653fc42cb2eacd436e8fd
adonis <adonis.lau.dev@gmail.com>
2018-05-27 23:46:48 +0800

    异常处理

---
+++ src/main/scala/bid/adonis/lau/ExceptionHandling.scala
@@ -0,0 +1,19 @@
+package bid.adonis.lau
+
+import java.io.{FileNotFoundException, FileReader, IOException}
+
+object ExceptionHandling {
+
+  def main(args: Array[String]): Unit = {
+    try {
+      val f = new FileReader("input.txt")
+    } catch {
+      case ex:FileNotFoundException =>
+        println("Missing file exception")
+      case ex: IOException =>
+        println("IO Exception")
+    } finally {
+      println("Exiting finally...")
+    }
+  }
+}
